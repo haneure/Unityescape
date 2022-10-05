d5711738f5f204ff476ccf9bfd15e63e7f140ec1
Christian Halim <chris.richard.halim@gmail.com>
2022-10-05 19:06:16 +0700

    Add Tutorial Level
    - Nambah level tutorial, posisi awalnya kupindah
    - Nambah canvas yang bisa dipake buat kasi UI di world space
    - Nambah sistem inventory (sementara cuma ada 5 slot; notes: Belum handle kalo inventory udah 5 gabisa nambah lagi)
    - Nambah beberapa asset baru, link ada di google docs
    - Change interact icon GrabCursor -> whole hand (terlihat lebih clean)

---
+++ Assets/Script/Jailbar/t_jailbar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t_jailbar : MonoBehaviour
+{
+    Inventory inventory;
+    public bool haveKey = false;
+    GameObject keyhole;
+    public UI_Inventory inventori;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.Find("FirstPersonController").GetComponent<Inventory>();
+        keyhole = this.gameObject;
+        Debug.Log(inventori.inventory);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openJailbar() {
+        foreach(var item in inventory.inventory) {
+            if(item.name == "rust_key"){
+                haveKey = true;
+            } else {
+                Debug.Log("rust_key is not in your inventory");
+            }
+            // Debug.Log(inventorySlot);
+        }
+
+        if(haveKey) {
+            keyhole.SetActive(false);
+            inventori.deleteFromInventory("rust_key");
+        } else {
+            Debug.Log("Kamu tidak punya kunci");
+        }
+    }
+}

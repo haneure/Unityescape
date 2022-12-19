4143d9c07406e3d23289aaba462d08ac8f5fa67f
Christian Halim <chris.richard.halim@gmail.com>
2022-12-20 00:44:37 +0700

    Add prefab UI, zombie dan PlayerScene6 (kalo mau pake prefab ini rename jadi Player). sfx, gameover and fix menu

--- Assets/Script/MenuUI/PauseMenu.cs
+++ Assets/Script/MenuUI/PauseMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public string leaveGame = "Menu";
-    public static bool GameIsPaused = false;
+    public bool GameIsPaused = false;
     public FirstPersonController fps;
 
     public GameObject pauseMenuUI;

3c06496418060aca3d81f3caa14232bff0712b09
Christian Halim <chris.richard.halim@gmail.com>
2022-12-21 01:11:29 +0700

    fix cutscene dan add some rocks in scene 7

--- Assets/Script/Items/Rock/t_rock.cs
+++ Assets/Script/Items/Rock/t_rock.cs
@@ -16,7 +16,7 @@ public class t_rock : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rockAudio.spatialBlend = 1;
     }
 
     // Update is called once per frame
